e12cfebde790a4ed44104287ff2b10566cf7ce9b
yusshu <yushu@unnamed.team>
2021-07-09 21:21:38 -0500

    add documentation for CallableBinding and ObjectBinding

--- molang/src/main/java/team/unnamed/molang/binding/CallableBinding.java
+++ molang/src/main/java/team/unnamed/molang/binding/CallableBinding.java
@@ -1,7 +1,17 @@
 package team.unnamed.molang.binding;
 
+/**
+ * Interface for bindings that can be
+ * called like functions, some default
+ * callable bindings may be the trigonometric
+ * functions 'math.cos', 'math.sin', etc...
+ */
 public interface CallableBinding {
 
+    /**
+     * Executes this callable binding
+     * using the given {@code arguments}
+     */
     Object call(Object... arguments);
 
 }
--- molang/src/main/java/team/unnamed/molang/binding/ObjectBinding.java
+++ molang/src/main/java/team/unnamed/molang/binding/ObjectBinding.java
@@ -1,7 +1,17 @@
 package team.unnamed.molang.binding;
 
+/**
+ * Represents an object-like binding,
+ * these objects can have properties
+ * (or fields) that can be read and
+ * sometimes written
+ */
 public interface ObjectBinding {
 
+    /**
+     * Gets the property value in this
+     * object with the given {@code name}
+     */
     Object getProperty(String name);
 
 }
