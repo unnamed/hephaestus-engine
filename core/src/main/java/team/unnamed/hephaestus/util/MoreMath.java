93d45d17e8fc17f2d089d62e70ded966b716d2c2
yusshu <yushu@unnamed.team>
2021-10-20 17:55:39 -0500

    chore: remove unused method from MoreMath utility

--- core/src/main/java/team/unnamed/hephaestus/util/MoreMath.java
+++ core/src/main/java/team/unnamed/hephaestus/util/MoreMath.java
@@ -10,15 +10,6 @@ public final class MoreMath {
         throw new UnsupportedOperationException("This class cannot be instantiated");
     }
 
-    /**
-     * Clamps the given {@code value} between
-     * the given {@code min} and {@code max}
-     * bounds
-     */
-    public static double clamp(double value, double min, double max) {
-        return Math.min(Math.max(value, min), max);
-    }
-
     public static float shrink(float p) {
         return 3.2F + 0.6F * p;
     }
