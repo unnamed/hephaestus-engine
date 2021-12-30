fd2d94454f206772f576f11c9f23d6dd7d323209
yusshu <yushu@unnamed.team>
2021-12-30 00:27:38 -0500

    fix: add (temporary) default value for 'small' parameter

--- core/src/main/java/team/unnamed/hephaestus/reader/BBModelReader.java
+++ core/src/main/java/team/unnamed/hephaestus/reader/BBModelReader.java
@@ -373,7 +373,7 @@ public class BBModelReader implements ModelReader {
         // instantiate bone and asset with empty and mutable
         // cubes, bones and assets, they'll be filled later
         ModelBoneAsset asset = new ModelBoneAsset(name, pivot, cursor.next(), cubes, boneAssets);
-        ModelBone bone = new ModelBone(parent, name, rotation, bones, offset, asset.getCustomModelData());
+        ModelBone bone = new ModelBone(parent, name, rotation, bones, offset, false, asset.getCustomModelData());
 
         for (JsonElement childElement : json.get("children").getAsJsonArray()) {
             if (childElement.isJsonObject()) {
