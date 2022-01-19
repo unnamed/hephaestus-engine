94cf2f6f177000b87f460f35f43f90d43a246bb0
yusshu <yushu@unnamed.team>
2022-01-18 22:35:54 -0500

    test: remove AI from Redstone Monstrosity testing entity

--- runtime-minestom/src/test/java/team/unnamed/hephaestus/minestom/RedstoneMonstrosityView.java
+++ runtime-minestom/src/test/java/team/unnamed/hephaestus/minestom/RedstoneMonstrosityView.java
@@ -1,22 +1,12 @@
 package team.unnamed.hephaestus.minestom;
 
 import net.minestom.server.entity.EntityType;
-import net.minestom.server.entity.ai.goal.FollowTargetGoal;
-
-import java.time.Duration;
-import java.util.List;
 
 public class RedstoneMonstrosityView
         extends MinestomModelView {
 
     public RedstoneMonstrosityView() {
         super(EntityType.SLIME, Models.REDSTONE_MONSTROSITY);
-        addAIGroup(
-                List.of(
-                        new FollowTargetGoal(this, Duration.ofSeconds(2))
-                ),
-                List.of()
-        );
     }
 
     @Override
