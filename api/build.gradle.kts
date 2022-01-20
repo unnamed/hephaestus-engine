4d308f05c3ee37a10e0fcfd2beb6234b9ebdf5c4
yusshu <yushu@unnamed.team>
2022-01-20 16:34:56 -0500

    deps: update creative to 0.1.7-SNAPSHOT

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -1,7 +1,7 @@
 dependencies {
     api("org.jetbrains:annotations:22.0.0")
     api("com.google.code.gson:gson:2.8.0")
-    api("team.unnamed:creative-api:0.1.6-SNAPSHOT")
+    api("team.unnamed:creative-api:0.1.7-SNAPSHOT")
 
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.3.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.3.1")
