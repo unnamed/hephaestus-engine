1cf377e128d9cd3904d16d63c3f0d496864c791c
yusshu <yushu@unnamed.team>
2022-01-20 17:15:29 -0500

    feat: include runtime-bukkit

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,15 +3,15 @@ rootProject.name = "hephaestus-parent"
 includePrefixed("api")
 // includePrefixed("plugin")
 
-// includePrefixed("runtime-bukkit")
-// arrayOf(
-//         "v1_14_R1",
-//         "v1_15_R1",
-//         "v1_16_R3",
-//         "v1_17_R1"
-// ).forEach {
-//     include(":runtime-bukkit:adapt-$it")
-// }
+includePrefixed("runtime-bukkit")
+arrayOf(
+    "v1_14_R1",
+    "v1_15_R1",
+    "v1_16_R3",
+    "v1_17_R1"
+).forEach {
+    include(":runtime-bukkit:adapt-$it")
+}
 
 includePrefixed("runtime-minestom")
 
