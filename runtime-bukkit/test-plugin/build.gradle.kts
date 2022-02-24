55cfe12ebfc30ad7e14acb6a4bfb939e16c2e301
yusshu <yushu@unnamed.team>
2022-02-23 23:30:47 -0500

    build(runtime-bukkit): exclude gson from blockbench reader on test-plugin, gson is already provided by bukkit

--- runtime-bukkit/test-plugin/build.gradle.kts
+++ runtime-bukkit/test-plugin/build.gradle.kts
@@ -5,7 +5,9 @@ plugins {
 dependencies {
     implementation(project(":runtime-bukkit:api"))
     implementation(project(":runtime-bukkit:adapt-v1_18_R1", "reobf"))
-    implementation(project(":hephaestus-reader-blockbench"))
+    implementation(project(":hephaestus-reader-blockbench")) {
+        exclude(group = "com.google.code.gson", module = "gson")
+    }
 
     compileOnly("io.papermc.paper:paper-api:1.18.1-R0.1-SNAPSHOT")
 }
