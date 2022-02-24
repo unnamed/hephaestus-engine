31bdafcd2deaeb13671db10e1c970ef83f316178
yusshu <yushu@unnamed.team>
2022-02-23 23:24:41 -0500

    fix(runtime-bukkit): specify 'reobf' configuration when including adapt-v1_18_R1 subproject

--- runtime-bukkit/test-plugin/build.gradle.kts
+++ runtime-bukkit/test-plugin/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 dependencies {
     implementation(project(":runtime-bukkit:api"))
-    implementation(project(":runtime-bukkit:adapt-v1_18_R1"))
+    implementation(project(":runtime-bukkit:adapt-v1_18_R1", "reobf"))
     implementation(project(":hephaestus-reader-blockbench"))
 
     compileOnly("io.papermc.paper:paper-api:1.18.1-R0.1-SNAPSHOT")
