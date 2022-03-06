6057a2887582a2b4eaa488089d7d1c3a3ee7138d
yusshu <yushu@unnamed.team>
2022-03-06 07:19:59 -0500

    build: prepare for publishing to our maven public repositories

--- buildSrc/src/main/kotlin/hephaestus.publishing-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/hephaestus.publishing-conventions.gradle.kts
@@ -3,7 +3,23 @@ plugins {
     `maven-publish`
 }
 
+val snapshotRepository: String by project
+val releaseRepository: String by project
+
 publishing {
+    repositories {
+        maven {
+            val isSnapshot = project.version.toString().endsWith("-SNAPSHOT")
+
+            url = uri(if (isSnapshot) { snapshotRepository } else { releaseRepository })
+            credentials {
+                username = project.properties["UNNAMED_REPO_USER"] as String?
+                    ?: System.getenv("REPO_USER")
+                password = project.properties["UNNAMED_REPO_PASSWORD"] as String?
+                    ?: System.getenv("REPO_PASSWORD")
+            }
+        }
+    }
     publications {
         create<MavenPublication>("maven") {
             from(components["java"])
