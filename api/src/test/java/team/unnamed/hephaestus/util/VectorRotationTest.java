d01d6a886245ba5ca2d14251d53c366a5b71e87e
yusshu <yushuverduras1309@gmail.com>
2023-10-08 17:17:01 -0500

    tests: add test on multiple axis rotation

--- api/src/test/java/team/unnamed/hephaestus/util/VectorRotationTest.java
+++ api/src/test/java/team/unnamed/hephaestus/util/VectorRotationTest.java
@@ -71,4 +71,68 @@ class VectorRotationTest {
         );
     }
 
+    private static void assertRotation(
+            Vector3Float expected,
+            Vector3Float vector,
+            Vector3Float rotation
+    ) {
+        Vector3Float rotated = Vectors.rotateDegrees(vector, rotation);
+        assertTrue(
+                Vectors.equals(expected, rotated, 0.000001D),
+                "Vectors should be equal, expected:  " + expected + " got: " + rotated
+        );
+    }
+
+    @Test
+    void test_rotation_in_multiple_axis() {
+        // in single axis (X)
+        assertRotation(
+                new Vector3Float(0, -1, 0),
+                new Vector3Float(0, 0, 1),
+                new Vector3Float(90, 0, 0)
+        );
+        // in single axis (Y)
+        assertRotation(
+                new Vector3Float(0, 0, -1),
+                new Vector3Float(1, 0, 0),
+                new Vector3Float(0, 90, 0)
+        );
+        // in single axis (Z)
+        assertRotation(
+                new Vector3Float(-1, 0, 0),
+                new Vector3Float(0, 1, 0),
+                new Vector3Float(0, 0, 90)
+        );
+
+        // in multiple axis (X,Y)
+        assertRotation(
+                new Vector3Float(1, 0, 0),
+                new Vector3Float(0, 1, 0),
+                new Vector3Float(90, 90, 0)
+        );
+        // in multiple axis (X,Z)
+        assertRotation(
+                new Vector3Float(0, 0, 1),
+                new Vector3Float(0, 1, 0),
+                new Vector3Float(90, 0, 90)
+        );
+        // in multiple axis (Y,Z)
+        assertRotation(
+                new Vector3Float(0, 1, 0),
+                new Vector3Float(0, 0, 1),
+                new Vector3Float(0, 90, 90)
+        );
+        // in multiple axis (X,Y,Z)
+        assertRotation(
+                new Vector3Float(0, 0, -1),
+                new Vector3Float(1, 0, 0),
+                new Vector3Float(90, 90, 90)
+        );
+        assertRotation(
+                new Vector3Float(0.5F, 0.5F, -0.70710677F),
+                new Vector3Float(1, 0, 0),
+                new Vector3Float(45, 45, 45)
+        );
+    }
+
 }
