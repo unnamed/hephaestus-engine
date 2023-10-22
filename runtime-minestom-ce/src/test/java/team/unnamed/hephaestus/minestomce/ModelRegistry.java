3a4e0014da7360f97de6349ef945d8b9b5ca0f70
yusshu <yushu@unnamed.team>
2023-10-22 16:03:23 -0500

    tests: make model registry generate a shorter id
    also puts the most variable part at the start, helpful for tab-completion

--- runtime-minestom-ce/src/test/java/team/unnamed/hephaestus/minestomce/ModelRegistry.java
+++ runtime-minestom-ce/src/test/java/team/unnamed/hephaestus/minestomce/ModelRegistry.java
@@ -94,7 +94,16 @@ public final class ModelRegistry {
     }
 
     public static String generateViewId() {
-        return Long.toHexString(System.nanoTime());
+        // generate a hexadecimal string from the current time in milliseconds
+        final String hexMillis = Long.toHexString(System.currentTimeMillis());
+        final char[] chars = hexMillis.toCharArray();
+        // reverse the array
+        for (int i = 0; i < chars.length / 2; i++) {
+            char tmp = chars[i];
+            chars[i] = chars[chars.length - 1 - i];
+            chars[chars.length - 1 - i] = tmp;
+        }
+        return new String(chars);
     }
 
 }
