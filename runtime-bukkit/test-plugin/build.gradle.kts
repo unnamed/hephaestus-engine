31214daf0205864418a2146ed236814a7eec1252
yusshu <yushu@unnamed.team>
2023-12-15 00:53:52 -0500

    fix(runtime-bukkit/test-plugin): exclude dependencies that the server already has, update creative-central

--- runtime-bukkit/test-plugin/build.gradle.kts
+++ runtime-bukkit/test-plugin/build.gradle.kts
@@ -8,11 +8,23 @@ dependencies {
     compileOnly("io.papermc.paper:paper-api:1.20.2-R0.1-SNAPSHOT")
 
     // resource-pack plugin api
-    compileOnly("team.unnamed:creative-central-api:0.7.2-SNAPSHOT")
+    compileOnly("team.unnamed:creative-central-api:0.10.0-SNAPSHOT")
 
     // hephaestus-engine dependencies
     implementation(project(":hephaestus-api"))
     implementation(project(":hephaestus-reader-blockbench"))
     implementation(project(":hephaestus-runtime-bukkit-api"))
-    implementation(project(":hephaestus-runtime-bukkit-adapt-v1_20_R2"))
+    implementation(project(":hephaestus-runtime-bukkit-adapt-v1_20_R2", configuration = "reobf"))
+}
+
+tasks {
+    shadowJar {
+        dependencies {
+            exclude(dependency("team.unnamed:creative-api"))
+            exclude(dependency("com.google.code.gson:gson"))
+            exclude(dependency("net.kyori:adventure-.*"))
+            exclude(dependency("net.kyori:examination-.*"))
+            exclude(dependency("org.jetbrains:annotations"))
+        }
+    }
 }
