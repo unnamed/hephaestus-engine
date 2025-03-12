2ba778ac3c96c675871d01a2122734e00c38a775
yusshu <andreroldan.me@gmail.com>
2025-03-12 07:55:43 -0500

    chore: update licenses to 2025

--- runtime-minestom/src/main/java/team/unnamed/hephaestus/minestom/GenericBoneEntity.java
+++ runtime-minestom/src/main/java/team/unnamed/hephaestus/minestom/GenericBoneEntity.java
@@ -1,7 +1,7 @@
 /*
  * This file is part of hephaestus-engine, licensed under the MIT license
  *
- * Copyright (c) 2021-2023 Unnamed Team
+ * Copyright (c) 2021-2025 Unnamed Team
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
